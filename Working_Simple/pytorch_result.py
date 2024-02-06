abf8c5512be3f79442a8f8707d6069da1566d893
AthomsG <47532248+AthomsG@users.noreply.github.com>
2024-02-06 22:31:47 +0000

    simple discrete tac working. run pytorch_result.py

--- Working_Simple/pytorch_result.py
+++ Working_Simple/pytorch_result.py
@@ -7,15 +7,14 @@ import os
 from environment import test_env
 from networks import Actor, Critic
 
-
 # set seeds for reproducibility
 seed = 0
 torch.manual_seed(seed)
 np.random.seed(seed)
 import random; random.seed(seed)
 
-n_actions    = 100
-n_iterations = 1
+n_actions    = 4
+n_iterations = 10000
 
 discount = 0.99
 lambd    = 0.1
@@ -32,19 +31,6 @@ critic = Critic(n_actions)
 # optimizers
 critic_opt = torch.optim.Adam(critic.parameters(), lr=lr)
 actor_opt  = torch.optim.Adam(actor.parameters() , lr=lr)
-
-# load weights into the actor and critic models
-actor.load_state_dict(torch.load('actor_weights.pth'))
-critic.load_state_dict(torch.load('critic1_weights.pth'))
-
-# check weights match analytical_result's
-print("Actor Network Parameters:")
-for name, param in actor.state_dict().items():
-    print(name, param)
-
-print("\nCritic Network Parameters:")
-for name, param in critic.state_dict().items():
-    print(name, param)
 
 # test environment
 env = test_env(n_actions)
@@ -78,22 +64,11 @@ for i in range(n_iterations):
     # Critic
     critic_opt.zero_grad()
     v_loss.backward()
-
-    print('\n\Critic Gradients:\n\n')
-    for name, param in critic.named_parameters():
-        if param.requires_grad:
-            print(name, param.grad)
-
     critic_opt.step()
+
     # Actor
     actor_opt.zero_grad()
     p_loss.backward()
-
-    print('\n\nActor Gradients:\n\n')
-    for name, param in actor.named_parameters():
-        if param.requires_grad:
-            print(name, param.grad)
-
     actor_opt.step()
 
     state = next_state
