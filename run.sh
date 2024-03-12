e4a0f0a9838cafcfad388e674e9cc1ffcd2cc676
AthomsG <47532248+AthomsG@users.noreply.github.com>
2024-03-12 19:40:09 +0000

    updated run script to take as input the GPU number

--- run.sh
+++ run.sh
@@ -5,9 +5,18 @@ mkdir -p train_log
 mkdir -p train_log/$1 # $1 is the environment name that is passed
 
 # define an array of values to be used in experiments
-seeds=(0)
+seeds=(0 1)
 lambd=(0.1)
-alpha=(1 2 5)
+alpha=(1 1.5 2 5)
+
+# array to store process IDs
+pids_file="pids.txt"
+
+# remove existing pids file if exists
+rm -f "$pids_file"
+
+# specify the GPU you want to use, default is 0
+export CUDA_VISIBLE_DEVICES=${2:-0}
 
 for seed in "${seeds[@]}"; do
     for lam in "${lambd[@]}"; do
@@ -15,11 +24,11 @@ for seed in "${seeds[@]}"; do
             # define the log file where terminal outputs are stored
             log_dir="train_log/$1/temp_${lam}_alpha_${alp}_seed_${seed}.log"
             # run Python script and redirect the output to log file
-            python -u train.py --game_name $1 --alpha $alp --lambd $lam --seed $seed --num_iterations 100 --learning_starts 32 > $log_dir 2>&1 &
+            python -u train.py --game_name $1 --alpha $alp --lambd $lam --seed $seed --num_iterations 3000000 --learning_starts 5000 --update_target_every 2500 > $log_dir 2>&1 &
+            # store the PID of the last background process and write it to the pids file
+            echo $! >> "$pids_file"
         done
     done
 done
 
 echo "All processes have started!"
-
-# pkill -9 -f "train.py --game $1" # to kill all started processes
