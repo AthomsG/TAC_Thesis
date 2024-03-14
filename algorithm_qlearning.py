e5c6095ec5f134ec8e6ca14392e3dbe8abfdc5f1
AthomsG <47532248+AthomsG@users.noreply.github.com>
2024-03-14 11:45:16 +0000

    removed debug line from code

--- algorithm_qlearning.py
+++ algorithm_qlearning.py
@@ -196,7 +196,6 @@ class RAC:
             # defined between equations (9) and (10)
             targets = reward_batch.squeeze(1) + (1 - done_batch.squeeze(1)) * self.discount * (next_action_qvals + self.lambd * next_action_log_alpha)
 
-            from IPython import embed; embed()
         action_qvals = qvals.gather(1, action_batch).squeeze(1)
         # compute value loss (equation 9)
         v_loss = F.mse_loss(action_qvals, targets)/2
